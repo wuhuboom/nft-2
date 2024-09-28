f70c74e14c2bee76aef2f2af3862cc1f1fd1da34
attoote <119163844+attoote@users.noreply.github.com>
2024-09-28 13:33:39 +0700

    fix

--- src/views/market.vue
+++ src/views/market.vue
@@ -352,6 +352,24 @@
       </ul>
     </van-popup>
     <activationCode />
+    <van-popup class="no-money-pop" v-model="moneyShow" position="center">
+      <ul class="font14 desc-money-list center-center flex-column color-fff">
+        <li class="m-b-8">{{ $t(`backapi.balanceNotEnough`) }}</li>
+        <li
+          class="recharge center-center m-t-16 m-b-16"
+          @click="
+            $router.push({
+              name: 'Recharge',
+            })
+          "
+        >
+          {{ $t(`recharge.button.now.text`) }}
+        </li>
+        <li class="active" @click="moneyShow = false">
+          {{ $t(`cancel.btn.text`) }}
+        </li>
+      </ul>
+    </van-popup>
   </div>
 </template>
 
@@ -379,6 +397,7 @@ export default {
   components: { activationCode, HomeTopBar, MoneyBar },
   data() {
     return {
+      moneyShow: true,
       errIcon,
       ritIcon,
       formData: {
@@ -677,6 +696,29 @@ export default {
   }
   .tips-radio {
     align-items: flex-start;
+  }
+}
+.no-money-pop {
+  width: 246px;
+  height: 162px;
+  border-radius: 14px;
+  border: solid 1px rgba(0, 0, 0, 0);
+  background-image: linear-gradient(to bottom, #1f2430, #151515);
+  .desc-money-list {
+    height: 100%;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    text-align: center;
+    padding: 0 20px;
+    .recharge {
+      width: 181px;
+      height: 33px;
+      border-radius: 12px;
+      box-shadow: 0 3px 6px 0 #5a3e01;
+      background-color: #f5673e;
+    }
   }
 }
 </style>
