8f374d273c21418092d03535514ea6d22a864f54
attoote <119163844+attoote@users.noreply.github.com>
2024-11-22 20:53:51 +0700

    fix

--- src/views/pagesinvest.vue
+++ src/views/pagesinvest.vue
@@ -1,9 +1,8 @@
 <template>
-  <div class="pagesinvest-page font12 p-l-12 p-r-12">
+  <div class="pagesinvest-page font12">
     <AppTopBar
       :titleClass="['app-top-black-title']"
-      :topBarTitle="$t('invest.history.text')"
-      :styleObj="{ backgroundColor: 'tra' }"
+      :topBarTitle="$t('invest.record.page.text')"
     >
       <template #right>
         <i
@@ -24,29 +23,63 @@
       </li>
     </ul>
     <NoData v-if="finished && !video.length" />
-    <LoadList class="" :onload="informationVideo" :finished="finished">
-      <div
-        class="plans"
-        v-for="(item, idx) in video"
-        :key="idx"
-        @click="goDetail(item)"
+    <div class="p-l-12 p-r-12">
+      <LoadList
+        style="margin-top: 0 !important"
+        :onload="informationVideo"
+        :finished="finished"
       >
-        <pagesinvestItem :open="open" :item="item" />
-      </div>
-    </LoadList>
+        <div
+          class="plans"
+          v-for="(item, idx) in video"
+          :key="idx"
+          @click="goDetail(item)"
+        >
+          <pagesinvestItem :open="open" :item="item" />
+        </div>
+      </LoadList>
+    </div>
   </div>
 </template>
 
 <script>
+import pagesinvestItem from "@/views/pagesinvestItem";
 import i18n from "@/locale";
 import userApi from "@/api/user";
 import dayjs from "dayjs";
-import pagesinvestItem from "@/views/pagesinvestItem";
 export default {
   name: "balanceRecordView",
   data() {
     return {
       open: false,
+      navs: [
+        {
+          name: i18n.t(`property.record.search.time1.text`),
+          key: 0,
+        },
+        {
+          name: i18n.t(`property.record.search.time2.text`),
+          key: 4,
+        },
+        {
+          name: i18n.t(`property.record.search.time3.text`),
+          key: 2,
+        },
+        {
+          name: i18n.t(`property.record.search.time5.text`),
+          key: 3,
+        },
+      ],
+      dataArray: [
+        {
+          label: i18n.t("invest.record.status0.text"),
+          value: 0,
+        },
+        {
+          label: i18n.t("Today.History"),
+          value: 1,
+        },
+      ],
       finished: false,
       video: [],
       query: {
@@ -80,54 +113,23 @@ export default {
           value: 2,
         },
       ],
-      dataArray: [
-        {
-          label: i18n.t("invest.record.status0.text"),
-          value: 0,
-        },
-        {
-          label: i18n.t("Today.History"),
-          value: 1,
-        },
-      ],
     };
   },
   components: {
     pagesinvestItem,
   },
   methods: {
-    //普通盈利 每天的收益
-    way1earnings(doc, key = "min") {
-      const base = +(doc.fixed || 0);
-      const val = doc[`${key}`] || 0;
-      if (val > 0) {
-        const curRate = doc.rate / 100;
-        let num = val * curRate * 1 + base;
-        return num;
-      } else {
-        return 0;
-      }
-    },
-    moneyFn(v) {
-      if (this.open === false) {
-        return "*******";
-      } else if (this.open === true) {
-        return v;
-      }
-      return v;
-    },
-    dayFn(v) {
-      if (this.open === false) {
-        return "**";
-      } else if (this.open === true) {
-        return v;
-      }
-      return v;
-    },
     changeOpen() {
       this.open = !this.open;
     },
+    chosen(v) {
+      this.query.status = v;
+      this.query.pageNo = 1;
+      this.video = [];
+      this.informationVideo();
+    },
     goDetail(item) {
+      //window.localStorage.setItem("recroedItem", JSON.stringify(item));
       this.$router.push({
         path: "/pages/invest/recordDetail?id=" + item.id,
       });
@@ -135,15 +137,12 @@ export default {
     date(t) {
       return dayjs.unix(this.$ToSeconds(t)).format("YYYY-MM-DD HH:mm");
     },
-    count(item) {
-      const val = this.divide(item.money) || 0;
+    count(doc) {
+      const val = this.divide(doc.money) || 0;
+      const base = +(doc.fixed || 0);
       if (val > 0) {
-        //= X * (1 + Y/100)*Z
-        const curRate = item.rate / 100;
-        let num = val * curRate * item.days;
-        if (+item.autoInvest === 1) {
-          num = val * Math.pow(1 + curRate, item.days) - val;
-        }
+        const curRate = doc.rate / 100;
+        let num = val * curRate * 1 + base; //天
         return num.toFixed(2);
       } else {
         return 0;
@@ -156,41 +155,38 @@ export default {
     },
     getType2(value) {
       let res = this.typeOptions2.find((item) => item.value == value);
+      if (!res) return "";
       return res.label;
-    },
-    chosen(v) {
-      this.query.status = v;
-      this.query.pageNo = 1;
-      this.video = [];
-      this.informationVideo();
     },
     async informationVideo(obj = {}) {
       const params = {
         ...this.query,
         ...obj,
       };
+      if (params.status === "") {
+        delete params.status;
+      }
       const [err, res] = await userApi.investMyPost(params);
       if (err) {
         this.finished = true;
         return false;
       }
       this.finished = res.data.results.length < this.query.pageSize;
-      // res.data.results 模拟数据
+      //模拟数据  res.data.results
       // res.data.results = [
       //   {
       //     id: 1,
       //     plan: {
-      //       name: "plan1",
+      //       name: "测试",
       //     },
       //     planIcon: "https://img.yzcdn.cn/vant/cat.jpeg",
-      //     type: 0,
-      //     status: 0,
-      //     days: 10,
-      //     money: 1000,
+      //     days: 1,
+      //     money: 100,
       //     autoInvest: 1,
-      //     rate: 10,
+      //     rate: 0.1,
       //     orderNo: "123456",
-      //     finishTime: 1620000000,
+      //     finishTime: 1617264000,
+      //     status: 1,
       //   },
       // ];
       this.video =
@@ -199,6 +195,22 @@ export default {
           : this.video.concat(res.data.results);
       this.query.pageNo++;
     },
+  },
+  created() {
+    if (this.$store.state.fromRoute.name === "RecordDetail") {
+      this.query = this.$store.state.recroed.query;
+      this.video = this.$store.state.recroed.video;
+    }
+  },
+  mounted() {
+    document.querySelector("body").classList.add("gray-bg-img");
+  },
+  destroyed() {
+    this.$store.commit("setRecroed", {
+      video: this.video,
+      query: this.query,
+    });
+    document.querySelector("body").classList.remove("gray-bg-img");
   },
 };
 </script>
